e25724ba8237a69dfaf877f0ef7fbca117d87465
yamasaki <yamasakipann0218@gmail.com>
2022-12-22 02:39:00 +0900

    discord

--- discord/bot.go
+++ discord/bot.go
@@ -1,3 +1,34 @@
 package discord
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/bwmarrin/discordgo"
+)
+
+func ConnectBot() {
+	discord, err := discordgo.New("Bot " + "authentication token")
+	if err != nil {
+		log.Println(err)
+	}
+
+	discord.AddHandler(onMessageCreate)
+	err = discord.Open()
+	if err != nil {
+		log.Println(err)
+	}
+
+	stopBot := make(chan os.Signal, 1)
+	signal.Notify(stopBot, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-stopBot
+
+	err = discord.Close()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return
+}
