7e93c94810c3bae770ff4b4fd8a80aec32a5eb5b
seipan <yamasakipann0218@gmail.com>
2023-01-02 23:47:33 +0900

    utils

--- logger/logger.go
+++ logger/logger.go
@@ -2,22 +2,22 @@ package logger
 
 type Fields map[string]interface{}
 
-func Info(Fields) {
+func Info(Fields, string) {
 
 }
 
-func Debug(Fields) {
+func Debug(Fields, string) {
 
 }
 
-func Warn(Fields) {
+func Warn(Fields, string) {
 
 }
 
-func Error(Fields) {
+func Error(Fields, string) {
 
 }
 
-func Panic(Fields) {
+func Panic(Fields, string) {
 
 }
