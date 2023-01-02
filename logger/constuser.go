e150cbc99ba27b8ea31c03ccdbbe878ae1f0dd39
seipan <yamasakipann0218@gmail.com>
2023-01-03 00:05:16 +0900

    Checkuser

--- logger/constuser.go
+++ logger/constuser.go
@@ -7,3 +7,11 @@ const (
 	AllUser   = "@everyone"
 	HereUser  = "@here"
 )
+
+func CheckUser(user string) bool {
+	if user == DebugUser || user == AdminUser || user == DevUser || user == AllUser || user == HereUser {
+		return true
+	}
+
+	return false
+}
