060e39fcca01fc7f2dc1733345ff193a79c8d5b1
seipan <yamasakipann0218@gmail.com>
2023-01-02 23:28:11 +0900

    logger

---
+++ logger/logger.go
@@ -0,0 +1,23 @@
+package logger
+
+type Fields map[string]interface{}
+
+func Info(Fields) {
+
+}
+
+func Debug(Fields) {
+
+}
+
+func Warn(Fields) {
+
+}
+
+func Error(Fields) {
+
+}
+
+func Panic(Fields) {
+
+}
