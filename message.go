e9f632abd51f8d2d81107504f55422f477aa4d3c
yamasaki <yamasakipann0218@gmail.com>
2023-02-15 03:47:02 +0900

    構成変更

---
+++ message.go
@@ -0,0 +1,25 @@
+package logdis
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func (b *Bot) onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	clientId := os.Getenv("CLIENT_ID")
+	u := m.Author
+	fmt.Printf("%20s %20s(%20s) > %s\n", m.ChannelID, u.Username, u.ID, m.Content)
+	if u.ID != clientId {
+		b.sendMessage(s, m.ChannelID, "msg")
+	}
+}
+
+func (b *Bot) sendMessage(s *discordgo.Session, channelID string, msg string) {
+	_, err := s.ChannelMessageSend(channelID, msg)
+	if err != nil {
+		log.Println("Error sending message: ", err)
+	}
+}
