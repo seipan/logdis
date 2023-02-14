579621422c09f2dedff8c73c64fef5e62f5d03a0
yamasaki <yamasakipann0218@gmail.com>
2023-02-15 03:54:54 +0900

    method name

--- logger.go
+++ logger.go
@@ -2,75 +2,76 @@ package logdis
 
 type Fields map[string]interface{}
 
-func (*Bot) Info(f Fields, message string, user string) {
+func (b *Bot) Info(f Fields, message string, user string) {
 	str := "Info : "
 	str += parse(f, message, user)
 
+	b.sendMessage(b.bot, "", str)
 }
 
-func (*Bot) Infoln(f Fields, message string, user string) {
+func (b *Bot) Infoln(f Fields, message string, user string) {
 	str := "Info : "
 	str += parse(f, message, user)
 	str += "\n"
 
 }
 
-func (*Bot) Debug(f Fields, message string, user string) {
+func (b *Bot) Debug(f Fields, message string, user string) {
 	str := "Debug : "
 	str += parse(f, message, user)
 
 }
 
-func (*Bot) Debugln(f Fields, message string, user string) {
+func (b *Bot) Debugln(f Fields, message string, user string) {
 	str := "Debug : "
 	str += parse(f, message, user)
 	str += "\n"
 
 }
 
-func (*Bot) Error(f Fields, message string, user string) {
+func (b *Bot) Error(f Fields, message string, user string) {
 	str := "Error : "
 	str += parse(f, message, user)
 
 }
 
-func (*Bot) Errorln(f Fields, message string, user string) {
+func (b *Bot) Errorln(f Fields, message string, user string) {
 	str := "Error : "
 	str += parse(f, message, user)
 	str += "\n"
 }
 
-func (*Bot) Warn(f Fields, message string, user string) {
+func (b *Bot) Warn(f Fields, message string, user string) {
 	str := "Warn : "
 	str += parse(f, message, user)
 
 }
 
-func (*Bot) Warnln(f Fields, message string, user string) {
+func (b *Bot) Warnln(f Fields, message string, user string) {
 	str := "Warn : "
 	str += parse(f, message, user)
 	str += "\n"
 }
 
-func (*Bot) Fatal(f Fields, message string, user string) {
+func (b *Bot) Fatal(f Fields, message string, user string) {
 	str := "Fatal : "
 	str += parse(f, message, user)
 
 }
 
-func (*Bot) Fatalln(f Fields, message string, user string) {
+func (b *Bot) Fatalln(f Fields, message string, user string) {
 	str := "Fatal : "
 	str += parse(f, message, user)
 	str += "\n"
 }
 
-func (*Bot) Panic(f Fields, message string, user string) {
+func (b *Bot) Panic(f Fields, message string, user string) {
 	str := "Panic : "
 	str += parse(f, message, user)
 
 }
 
-func (*Bot) Panicln(f Fields, message string, user string) {
+func (b *Bot) Panicln(f Fields, message string, user string) {
 	str := "Panic : "
 	str += parse(f, message, user)
 	str += "\n"
