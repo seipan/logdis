9237c5c18b8eb006a4d0c9c263a2bc742738aff6
yamasaki <yamasakipann0218@gmail.com>
2023-02-24 22:23:11 +0900

    example

--- example/main.go
+++ example/main.go
@@ -1,5 +1,16 @@
-package example
+package main
+
+import (
+	logdis "github.com/seipan/logdis"
+)
+
+const (
+	YOURTOKEN = "xxxxxxxxxxxx"
+)
 
 func main() {
-	
+	bot := logdis.NewDiscordBot(YOURTOKEN)
+	logdis.ConnectBot(bot)
+
+	bot.Infoln(nil, "hi my first message", logdis.AllUser)
 }
