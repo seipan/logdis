c54c311b9635de9687cb71dcbdcc630ce4c38cdf
yamasaki <yamasakipann0218@gmail.com>
2023-02-28 02:25:23 +0900

    edit example

--- example/main.go
+++ example/main.go
@@ -12,8 +12,9 @@ func main() {
 	bot := logdis.NewDiscordBot(YOURTOKEN)
 	logdis.ConnectBot(bot)
 
-	// if you
+	// If you want all members to know about the info level logs, you can do it this way
 	bot.Infoln(map[string]interface{}{"type": "my first message"}, "hi my first message", logdis.AllUser)
 
+	// If you want to let your original members know about the ERROR level log, you can do so by doing this
 	bot.Errorln(map[string]interface{}{"type": "my second message"}, "hi my second message", "devmember")
 }
