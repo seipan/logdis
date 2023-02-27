c2931116902c9bf30f714d8795a644c1368adc48
yamasaki <yamasakipann0218@gmail.com>
2023-02-28 01:45:45 +0900

    edit example

--- example/main.go
+++ example/main.go
@@ -12,5 +12,8 @@ func main() {
 	bot := logdis.NewDiscordBot(YOURTOKEN)
 	logdis.ConnectBot(bot)
 
+	// if you
 	bot.Infoln(map[string]interface{}{"type": "my first message"}, "hi my first message", logdis.AllUser)
+
+	bot.Errorln(map[string]interface{}{"type": "my second message"}, "hi my second message", "devmember")
 }
