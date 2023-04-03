d2fbf4dc5ca28f2c12f37b389e0f937d25dc06b5
yamasaki <yamasakipann0218@gmail.com>
2023-04-03 15:47:08 +0900

    fix: method rename

--- logger.go
+++ logger.go
@@ -2,78 +2,78 @@ package logdis
 
 type Fields map[string]interface{}
 
-func (b *Client) Info(f Fields, message string, user string) {
+func (b *Logger) Info(f Fields, message string, user string) {
 	str := "Info : "
 	str += parse(f, message, user)
 
 }
 
-func (b *Client) Infoln(f Fields, message string, user string) {
+func (b *Logger) Infoln(f Fields, message string, user string) {
 	str := "Info : "
 	str += parse(f, message, user)
 	str += "\n"
 
 }
 
-func (b *Client) Debug(f Fields, message string, user string) {
+func (b *Logger) Debug(f Fields, message string, user string) {
 	str := "Debug : "
 	str += parse(f, message, user)
 
 }
 
-func (b *Client) Debugln(f Fields, message string, user string) {
+func (b *Logger) Debugln(f Fields, message string, user string) {
 	str := "Debug : "
 	str += parse(f, message, user)
 	str += "\n"
 
 }
 
-func (b *Client) Error(f Fields, message string, user string) {
+func (b *Logger) Error(f Fields, message string, user string) {
 	str := "Error : "
 	str += parse(f, message, user)
 
 }
 
-func (b *Client) Errorln(f Fields, message string, user string) {
+func (b *Logger) Errorln(f Fields, message string, user string) {
 	str := "Error : "
 	str += parse(f, message, user)
 	str += "\n"
 
 }
 
-func (b *Client) Warn(f Fields, message string, user string) {
+func (b *Logger) Warn(f Fields, message string, user string) {
 	str := "Warn : "
 	str += parse(f, message, user)
 
 }
 
-func (b *Client) Warnln(f Fields, message string, user string) {
+func (b *Logger) Warnln(f Fields, message string, user string) {
 	str := "Warn : "
 	str += parse(f, message, user)
 	str += "\n"
 
 }
 
-func (b *Client) Fatal(f Fields, message string, user string) {
+func (b *Logger) Fatal(f Fields, message string, user string) {
 	str := "Fatal : "
 	str += parse(f, message, user)
 
 }
 
-func (b *Client) Fatalln(f Fields, message string, user string) {
+func (b *Logger) Fatalln(f Fields, message string, user string) {
 	str := "Fatal : "
 	str += parse(f, message, user)
 	str += "\n"
 
 }
 
-func (b *Client) Panic(f Fields, message string, user string) {
+func (b *Logger) Panic(f Fields, message string, user string) {
 	str := "Panic : "
 	str += parse(f, message, user)
 
 }
 
-func (b *Client) Panicln(f Fields, message string, user string) {
+func (b *Logger) Panicln(f Fields, message string, user string) {
 	str := "Panic : "
 	str += parse(f, message, user)
 	str += "\n"
