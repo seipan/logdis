c6626aed15323e861c17115b95e6621c7c4f10d2
yamasaki <yamasakipann0218@gmail.com>
2023-04-07 01:09:50 +0900

    fix : add comment out

--- struct.go
+++ struct.go
@@ -1,17 +1,25 @@
 package logdis
 
+// This structure defines what is needed to output logs to any channel on discord.
 type Logger struct {
-	Webhook   string
-	Img       string
-	Name      string
-	ChannelId string
+
+	// This is the webhook url of the channel for which you want to send notifications to the discord.
+	// ex) discord.com/api/webhooks/xxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	Webhook string
+
+	// This is the url of the icon image of the bot that sends notifications to the discord
+	// ex) https://cdn-ak.f.st-hatena.com/images/fotolife/h/hikiniku0115/20190806/20190806000644.png
+	Img string
+
+	// This is the name of the bot that will send notifications to the discord
+	// ex) hogehoge
+	Name string
 }
 
-func NewClient(webhook string, channnelId string, img string, name string) *Logger {
+func NewLogger(webhook string, img string, name string) *Logger {
 	return &Logger{
-		Webhook:   webhook,
-		Img:       img,
-		Name:      name,
-		ChannelId: channnelId,
+		Webhook: webhook,
+		Img:     img,
+		Name:    name,
 	}
 }
